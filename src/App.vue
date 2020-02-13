9847bce13b34d261141c3e23d12c6f079aad4c45
TheTiv <justin.gluck@vibe.co.com>
2020-02-13 15:00:12 +0000

    Animations are working as overlaps.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <header class="header">
+        <h2 class="site-title">GAA</h2>
+    </header>
+    <Workspace class="workspace" />
+    <Notifications class="notifications" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Workspace from './components/Workspace/Workspace'
+import Notifications from './components/Notifications/Notifications'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Workspace,
+    Notifications
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.header {
+  grid-area: header;
+  color: #005E82;
+}
+.workspace {
+  grid-area: main;
+  background-color: #005E82;
+  color: white;
+}
+.notifications {
+  grid-area: sidebar;
+  background-color: #3485AB;
+  color: white;
+  padding-left: 20px;
+  padding-right: 20px; 
+}
+
+.container {
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: auto 1fr;
+  grid-template-areas: 
+    "header header header"
+    "main main sidebar";
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -5,4 +5,31 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  data: {
+    sections: [
+        { title: 'Email', icon: 'mail', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Downloads', icon: 'downloads', description: 'Lorem Ipsum', link: '/'},
+        { title: 'GAA Newsletter', icon: 'newsletter', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Tickets', icon: 'tickets', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Club Map', icon: 'map', description: 'Lorem Ipsum', link: '/'},
+        { title: 'GMS', icon: 'doc', description: 'Lorem Ipsum', link: '/'},
+        { title: 'GRMA', icon: 'circle', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Child Welfare E - vetting', icon: 'globe', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Insurance', icon: 'padlock', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Membership Form', icon: 'person', description: 'Lorem Ipsum', link: '/'},
+        { title: 'GDPR Repository', icon: 'server', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Player Transfer System', icon: 'plane', description: 'Lorem Ipsum', link: '/'},
+        { title: 'MIS', icon: 'tent', description: 'Lorem Ipsum', link: '/'},
+        { title: 'Nat Club Draw', icon: 'shield', description: 'Lorem Ipsum', link: '/'},
+        { title: 'E-learning', icon: 'graduation', description: 'Lorem Ipsum', link: '/'},
+      ],
+    notifications: [
+    { title: 'article 1', date: '24-01-20', time:'7:01 pm', description: 'The deadline for submitting expenses is this Friday 11th October.' },
+    { title: 'article 2', date: '24-01-20', time:'7:01 pm', description: 'The deadline for submitting expenses is this Friday 11th October.' },
+    { title: 'article 3', date: '24-01-20', time:'7:01 pm', description: 'The deadline for submitting expenses is this Friday 11th October.' },
+    { title: 'article 4', date: '24-01-20', time:'7:01 pm', description: 'The deadline for submitting expenses is this Friday 11th October.' },
+    { title: 'article 5', date: '24-01-20', time:'7:01 pm', description: 'The deadline for submitting expenses is this Friday 11th October.' },
+    { title: 'article 6', date: '24-01-20', time:'7:01 pm', description: 'The deadline for submitting expenses is this Friday 11th October.' },    
+],
+}
 }).$mount('#app')
